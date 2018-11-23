4dc2c64b9bfebdad3a22d1885c04b3d3c84f5242
AqsaZahid256 <aqsazahid938@gmail.com>
2018-11-23 16:19:47 +0500

    add

---
+++ Last_Project/Last_Project/Controllers/ProductsTablesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Last_Project;
+
+namespace Last_Project.Controllers
+{
+    public class ProductsTablesController : Controller
+    {
+        private LastEntities db = new LastEntities();
+
+        // GET: ProductsTables
+        public ActionResult Index()
+        {
+            return View(db.ProductsTables.ToList());
+        }
+
+        // GET: ProductsTables/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ProductsTable productsTable = db.ProductsTables.Find(id);
+            if (productsTable == null)
+            {
+                return HttpNotFound();
+            }
+            return View(productsTable);
+        }
+
+        // GET: ProductsTables/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ProductsTables/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Product_Name,Product_Description,Minimum_Bid_Amount,Auction_Time,Category")] ProductsTable productsTable)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ProductsTables.Add(productsTable);
+                db.SaveChanges();
+                return RedirectToAction("Create");
+            }
+
+            return View(productsTable);
+        }
+
+        // GET: ProductsTables/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ProductsTable productsTable = db.ProductsTables.Find(id);
+            if (productsTable == null)
+            {
+                return HttpNotFound();
+            }
+            return View(productsTable);
+        }
+
+        // POST: ProductsTables/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Product_Name,Product_Description,Minimum_Bid_Amount,Auction_Time,Category")] ProductsTable productsTable)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(productsTable).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(productsTable);
+        }
+
+        // GET: ProductsTables/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ProductsTable productsTable = db.ProductsTables.Find(id);
+            if (productsTable == null)
+            {
+                return HttpNotFound();
+            }
+            return View(productsTable);
+        }
+
+        // POST: ProductsTables/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            ProductsTable productsTable = db.ProductsTables.Find(id);
+            db.ProductsTables.Remove(productsTable);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Last_Project/Last_Project/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Last_Project.Startup))]
+namespace Last_Project
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
